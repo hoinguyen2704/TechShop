f0d577fe42051323b729f5430368f7c1bb474c4e
hoinguyen2704 <hoinguyen.developer@gmail.com>
2026-02-18 19:34:01 +0700

    refactor: Remove import maps, introduce local package management, and streamline React application initialization.

--- index.tsx
+++ index.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
+// const rootElement = document.getElementById('root');
+// if (!rootElement) {
+//   throw new Error("Could not find root element to mount to");
+// }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
+// const root = ReactDOM.createRoot(rootElement);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>,
 );
